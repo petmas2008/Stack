828b800af66c25df840a247ad15e9fd3c8c4ae0b
petmas2008 <55663672+petmas2008@users.noreply.github.com>
2020-12-26 18:07:27 +0000

    update => working on changing directions

--- 3D_main.py
+++ 3D_main.py
@@ -1,6 +1,17 @@
+'''
+This is an attempt to re-create the ios game stack. Since pygame, to 
+my knowledge, doesn't have a builtin module to make 3D objects and dure to the fact 
+that I cannot be asked to find one on GitHub. I decided to 
+create a 3D illusion of a cuboid in which the programme is constantly drawing lines
+to portray this effect. To do this I needed the help of my brother so we could create
+some code which uses trigonometry to find the next coordinate based on the 
+length of the side, current position and angle representing the side. The programme is far from finished though
+'''
+
 import pygame
 import time
 import math
+from pygame.locals import *
 
 pygame.init()
 
@@ -50,80 +61,109 @@ def draw_cuboid(coords, depth):
     pygame.draw.line(screen, (255, 255, 255), (coords[3]), (coords[3][0], coords[3][1]+depth))
     pygame.draw.line(screen, (255, 255, 255), (coords[0]), (coords[0][0], coords[0][1]+depth))
 
-def change_direction(cur_direction):
-    if cur_direction == "SE":
-        return directions[3]
-    if cur_direction == "SW":
-        return directions[2]
-    if cur_direction == "NE":
-        return directions[1]
-    if cur_direction == "NW":
-        return directions[0]
+def change_direction(cur_direction, mode):
+    if mode == "edge":
+        if cur_direction == "SE":
+            return directions[3] # => NW
+        if cur_direction == "SW":
+            return directions[2] # => NE
+        if cur_direction == "NE":
+            return directions[1] # => SW
+        if cur_direction == "NW":
+            return directions[0] # => SE
+    elif mode == "space":
+        if cur_direction == "SE":
+            return directions[1] # => SW
+        if cur_direction == "SW":
+            return directions[0] # => SE
+        if cur_direction == "NE":
+            return directions[3] # => NW
+        if cur_direction == "NW":
+            return directions[2] # => NE
 
 def check_if_hit_edge(x, y, cur_direction):    
     global can_space
 
     if x+bx_change+x_change >= screen_width:
         can_space = True
-        return change_direction(cur_direction)
+        return change_direction(cur_direction, "edge")
     elif x == 0:
         can_space = True
-        return change_direction(cur_direction)
+        return change_direction(cur_direction, "edge")
     elif (x > 0) and (x < screen_width):
         return cur_direction
     
 
-def check_space_bar(cur_pos, s_pos, cur_direction):
+def check_space_bar(cur_pos, s_pos):
     global s_side1, s_side2
     global side1, side2
     global can_space
+    global score
+    global cur_direction
+    global root_coord
 
     x, y = cur_pos
     sx, sy = s_pos
     if can_space:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
+                print(cur_direction)
+                # cur_direction = change_direction(cur_direction, "space")
                 if cur_direction in ("SE", "NW"):
-                    cur_direction = change_direction(cur_direction)
                     if (x < sx) and (x+bx_change+x_change > sx):
                         side1 -= (sx - x)
-                    if (x > sx) and (x < sx+bx_change+x_change):
+                        score += 1
+                    elif (x > sx) and (x < sx+bx_change+x_change):
                         side1 -= ((x+bx_change+x_change) - (sx+bx_change+x_change))
+                        score += 1
+                    else:
+                         score = 0
+                if cur_direction in ("SW", "NE"):
+                    if (x < sx) and (x+bx_change+x_change > sx):
+                        side1 -= (sx - x)
+                        score += 1
+                    elif (x > sx) and (x < sx+bx_change+x_change):
+                        side1 -= ((x+bx_change+x_change) - (sx+bx_change+x_change))
+                        score += 1
+                    else:
+                         score = 0
+            print(cur_direction)
             can_space = False
-            print(side1, side2)
             generate_new_stack(cur_pos, side1, side2)
             return True
     else:
         return False
 
-def find_starting_coord(movement_speed, stack_x, stack_y, stack_depth):
+def find_starting_coord(movement_speed, stack_x, stack_y, stack_depth, required_dir):
     current_stack_pos = stack_x, stack_y-stack_depth
     while True:
-        current_stack_pos = adjust_pos(current_stack_pos[0], current_stack_pos[1], "NW")
-        if current_stack_pos[0] == 0:
-            return current_stack_pos
-        elif current_stack_pos[1] == 0:
-            return current_stack_pos
+        if required_dir == "SE":
+            current_stack_pos = adjust_pos(current_stack_pos[0], current_stack_pos[1], "NW")
+            if current_stack_pos[0] == 0:
+                return current_stack_pos
+            elif current_stack_pos[1] == 0:
+                return current_stack_pos
+        elif required_dir == "SW":
+            current_stack_pos = adjust_pos(current_stack_pos[0], current_stack_pos[1], "NE")
+            if current_stack_pos[0] == 0:
+                return current_stack_pos
+            elif current_stack_pos[1] == 0:
+                return current_stack_pos
 
 def generate_new_stack(pos, side1, side2):
     global previous_stack_info
     x, y = pos
     temp_info = []
-    print(previous_stack_info)
-    count = 0
     for info in previous_stack_info:
-        sx, sy = info[count]
+        sx, sy = info[0]
         temp_info.append(((sx, sy+s_depth), info[1], info[2]))
         temp_info.insert(0, ((sx, sy), side1, side2))
-        count += 1
     previous_stack_info = temp_info
 
 def analyse_info():
     for info in previous_stack_info:
         x, y = info[0]
-        if check_space_bar(root_coord, previous_stack_info[0][0], cur_direction) == True:
-            print("Hello")
-            print(previous_stack_info)
+        if check_space_bar(root_coord, previous_stack_info[0][0]) == True:
             previous_stack_coords.append(find_coords((x, y), s_angle1, s_angle2, info[1], info[2]))
         else:
             previous_stack_coords.append(find_coords((x, y), s_angle1, s_angle2, info[1], info[2]))
@@ -132,6 +172,8 @@ def analyse_info():
         draw_cuboid(stack_coords, s_depth)
     previous_stack_coords.clear()
 
+def draw_score():
+   screen.blit(text, (20, 20))
 
 running = True
 directions = ["SE", "SW", "NE", "NW"]
@@ -139,6 +181,10 @@ cur_direction = directions[0]
 can_space = True
 time_speed = 0.05
 movement_speed = 5
+
+score = 0
+font = pygame.font.SysFont("Rockwell", 32)
+text = font.render(str(score), True, (255, 255, 255))
 
 #~ Stack's rect attributes
 previous_stack_info = [((200, 250), 50, 60)]
@@ -153,7 +199,7 @@ s_depth = 20
 
 #~ Moving rects attributes
 #~ root_coord = (15, 40)
-root_coord = (find_starting_coord(movement_speed, s_root_coord[0], s_root_coord[1], s_depth))
+root_coord = (find_starting_coord(movement_speed, s_root_coord[0], s_root_coord[1], s_depth, "SE"))
 angle1 = 40
 angle2 = 80
 side1 = 50
@@ -169,7 +215,8 @@ while running:
     pygame.display.set_caption("Stack 3D")
     root_coord = adjust_pos(root_coord[0], root_coord[1], cur_direction)
     coords = find_coords(root_coord, angle1, angle2, side1, side2)
-    analyse_info()    
+    analyse_info()
+    draw_score()
     draw_cuboid(coords, depth)
     cur_direction = check_if_hit_edge(root_coord[0], root_coord[1], cur_direction)
     time.sleep(.05)
